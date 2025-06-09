bc4a35c2dd0140c72b161cc9e1e1d11f4cf911a7
Chris Engle <go2engle@gmail.com>
2025-06-09 09:41:37 -0400

    Implement code changes to enhance functionality and improve performance

--- dist/preload.js
+++ dist/preload.js
@@ -1,2 +1,68 @@
-(()=>{"use strict";var e={157:e=>{e.exports=require("electron")}},r={};function n(o){var t=r[o];if(void 0!==t)return t.exports;var i=r[o]={exports:{}};return e[o](i,i.exports,n),i.exports}(()=>{const e=n(157);e.contextBridge.exposeInMainWorld("electronAPI",{selectRepository:()=>e.ipcRenderer.invoke("select-repository"),selectBfgJar:()=>e.ipcRenderer.invoke("select-bfg-jar"),selectCloneDirectory:()=>e.ipcRenderer.invoke("select-clone-directory"),cloneRepository:r=>e.ipcRenderer.invoke("clone-repository",r),cleanRepository:r=>e.ipcRenderer.invoke("clean-repository",r),runPostCleaningCommands:r=>e.ipcRenderer.invoke("run-post-cleaning-commands",r),resetAndCleanup:r=>e.ipcRenderer.invoke("reset-and-cleanup",r)})})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "electron":
+/*!***************************!*\
+  !*** external "electron" ***!
+  \***************************/
+/***/ ((module) => {
+
+module.exports = require("electron");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry needs to be wrapped in an IIFE because it needs to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+/*!*****************************!*\
+  !*** ./src/main/preload.ts ***!
+  \*****************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+const electron_1 = __webpack_require__(/*! electron */ "electron");
+// Expose protected methods that allow the renderer process to use
+// the ipcRenderer without exposing the entire object
+electron_1.contextBridge.exposeInMainWorld('electronAPI', {
+    selectRepository: () => electron_1.ipcRenderer.invoke('select-repository'),
+    selectBfgJar: () => electron_1.ipcRenderer.invoke('select-bfg-jar'),
+    selectCloneDirectory: () => electron_1.ipcRenderer.invoke('select-clone-directory'),
+    cloneRepository: (options) => electron_1.ipcRenderer.invoke('clone-repository', options),
+    cleanRepository: (options) => electron_1.ipcRenderer.invoke('clean-repository', options),
+    runPostCleaningCommands: (options) => electron_1.ipcRenderer.invoke('run-post-cleaning-commands', options),
+    resetAndCleanup: (options) => electron_1.ipcRenderer.invoke('reset-and-cleanup', options)
+});
+
+})();
+
+/******/ })()
+;
 //# sourceMappingURL=preload.js.map
